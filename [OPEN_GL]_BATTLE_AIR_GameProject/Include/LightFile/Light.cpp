a5d967a8356456d9d7a66ceeb749ebd01b42e381
Mongzzi <89959473+Mongzzi@users.noreply.github.com>
2021-11-26 18:25:39 +0900

    Add Light Class(~Editing)

---
+++ [OPEN_GL]_BATTLE_AIR_GameProject/Include/LightFile/Light.cpp
@@ -0,0 +1,36 @@
+#include "Light.h"
+
+CLight::CLight()
+{
+}
+
+CLight::~CLight()
+{
+}
+
+bool CLight::Init()
+{
+	return false;
+}
+
+void CLight::Input(float fDeltaTime)
+{
+}
+
+int CLight::Update(float fDeltaTime)
+{
+	return 0;
+}
+
+int CLight::LateUpdate(float fDeltaTime)
+{
+	return 0;
+}
+
+void CLight::Collision(float fDeltaTime)
+{
+}
+
+void CLight::Render(float fDeltaTime)
+{
+}
