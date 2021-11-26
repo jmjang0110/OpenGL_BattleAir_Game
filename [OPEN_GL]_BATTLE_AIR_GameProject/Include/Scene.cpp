84c63604d33608f9d4aa6a5e34ca5648115b0bf0
jmjang0110 <90159618+jmjang0110@users.noreply.github.com>
2021-11-26 14:30:36 +0900

    Add Scene Class
    add Scene class

---
+++ [OPEN_GL]_BATTLE_AIR_GameProject/Include/Scene.cpp
@@ -0,0 +1,42 @@
+#include "Scene.h"
+#include "SceneFile/SceneManager.h"
+
+
+CScene::CScene()
+{
+	m_bEnable = false;
+
+
+}
+
+CScene::~CScene()
+{
+}
+
+bool CScene::Init()
+{
+	return false;
+}
+
+void CScene::Input(float fDeltaTime)
+{
+
+}
+
+int CScene::Update(float fDeltaTime)
+{
+	return 0;
+}
+
+int CScene::LateUpdate(float fDeltaTime)
+{
+	return 0;
+}
+
+void CScene::Collision(float fDeltaTime)
+{
+}
+
+void CScene::Render(float fDeltaTime)
+{
+}
