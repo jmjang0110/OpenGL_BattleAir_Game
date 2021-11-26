f62c72c74da01da13a6eda27b9a87d5655099ee6
jmjang0110 <90159618+jmjang0110@users.noreply.github.com>
2021-11-26 18:25:04 +0900

    Add Camera Class
    Add Camera Class

---
+++ [OPEN_GL]_BATTLE_AIR_GameProject/Include/CameraFile/Camera.cpp
@@ -0,0 +1,39 @@
+#include "Camera.h"
+
+
+
+
+CCamera::CCamera()
+{
+}
+
+CCamera::~CCamera()
+{
+}
+
+bool CCamera::Init(glm::vec3 CameraPos, glm::vec3 CameraDir, glm::vec3 CameraUp)
+{
+	return false;
+}
+
+void CCamera::Input(float fDeltaTime)
+{
+}
+
+int CCamera::Update(float fDeltaTime)
+{
+	return 0;
+}
+
+int CCamera::LateUpdate(float fDeltaTime)
+{
+	return 0;
+}
+
+void CCamera::Collision(float fDeltaTime)
+{
+}
+
+void CCamera::Render(float fDeltaTime)
+{
+}
---
+++ [OPEN_GL]_BATTLE_AIR_GameProject/Include/CameraFile/Camera.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../HeaderFile/Game.h"
+
+class CCamera
+{
+
+public:
+	CCamera();
+	~CCamera();
+
+
+private:
+	// 카메라 관련 변수
+	glm::vec3		m_cameraPos				= glm::vec3();	//--- 카메라 위치
+	glm::vec3		m_cameraDirection		= glm::vec3();	//--- 카메라 바라보는 방향
+	glm::vec3		m_cameraUp				= glm::vec3();  //--- 카메라 위쪽 방향
+
+
+public:
+
+
+	bool Init(glm::vec3 CameraPos, glm::vec3 CameraDir, glm::vec3 CameraUp);
+
+	void Input(float fDeltaTime);					// * 입력
+	int Update(float fDeltaTime);					// * 업데이트
+	int LateUpdate(float fDeltaTime);				// * 업데이트 후처리
+	void Collision(float fDeltaTime);				// * 충돌
+	void Render(float fDeltaTime);					// * 출력
+
+
+};
